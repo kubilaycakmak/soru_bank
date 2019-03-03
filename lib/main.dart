30c0c88f4dc79c7f916ad017d5215ff67b4e56d5
Kubilay Çakmak <kubilayckmk@gmail.com>
2019-03-04 01:45:10 +0300

    demo version v1
    added scrollable containers not show up,
    created main screen,
    created easy oop classification.

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:soru_bank/CardItemModel.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SoruBank());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SoruBank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, //will false when we finish debug
+      title: 'Soru Bank',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  var nowDate = DateTime.now();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  var appColors = [
+    Color.fromRGBO(231, 129, 109, 1.0),
+    Color.fromRGBO(99, 138, 223, 1.0),
+    Color.fromRGBO(111, 194, 173, 1.0)
+  ];
+  var cardIndex = 0;
+  ScrollController scrollController;
+  var currentColor = Color.fromRGBO(0, 139, 139, 1.0);
+
+  var cardsList = [
+    new CardItemModel("Matematik", Icons.multiline_chart, 3, 0.0),
+    new CardItemModel("Geometri", Icons.gps_not_fixed, 3, 5.0),
+    new CardItemModel("Matematik 2", Icons.gps_not_fixed, 3, 60.0)
+  ];
+
+  AnimationController animationController;
+  ColorTween colorTween;
+  CurvedAnimation curvedAnimation;
+
+  void initState() {
+    super.initState();
+    scrollController = new ScrollController();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: currentColor,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "Soru Bank",
+          style: TextStyle(fontSize: 20.0),
+        ),
+        backgroundColor: currentColor,
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () {
+                print(nowDate);
+              },
+            ), //will IconButton to clickable
+          ),
+        ],
+        elevation: 0.0, //changing on shadow
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 64.0, vertical: 2.0),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 45.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 12.0),
+                      child: Text(
+                        "Merhaba, Kullanıcı.",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w200),
+                      ),
+                    ),
+                    Text(
+                      "Haydi, Göster kendini.",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "Bugün senin için 3 yeni farklı dersten sorular hazırladık.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                  child: Text(
+                    "BUGÜN: " +
+                        nowDate.day.toString() +
+                        "/" +
+                        nowDate.weekday.toString() +
+                        "/" +
+                        nowDate.year.toString(), //get today date
+                    style: TextStyle(color: Colors.white, fontSize: 17.0),
+                  ),
+                ),
+                Container(
+                  height: 350.0,
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, position) {
+                      GestureDetector(
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Card(
+                            child: Container(
+                              width: 250.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Icon(
+                                          cardsList[position].icon,
+                                          color: appColors[position],
+                                        ),
+                                        Icon(
+                                          Icons.more_vert,
+                                          color: Colors.grey,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 8.0, vertical: 4.0),
+                                          child: Text(
+                                            "${cardsList[position].tasksRemaining} Sorular",
+                                            style:
+                                                TextStyle(color: Colors.grey),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(8.0),
+                                          child: LinearProgressIndicator(
+                                            value: cardsList[position]
+                                                .taskCompletion,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                          ),
+                        ),
+                        onHorizontalDragEnd: (details) {
+                          animationController = AnimationController(
+                              vsync: this,
+                              duration: Duration(milliseconds: 500));
+                          curvedAnimation = CurvedAnimation(
+                              parent: animationController,
+                              curve: Curves.fastOutSlowIn);
+                          animationController.addListener(() {
+                            setState(() {
+                              currentColor =
+                                  colorTween.evaluate(curvedAnimation);
+                            });
+                          });
+                          if (details.velocity.pixelsPerSecond.dx > 0) {
+                            if (cardIndex > 0) {
+                              cardIndex--;
+                              colorTween = ColorTween(
+                                  begin: currentColor,
+                                  end: appColors[cardIndex]);
+                            }
+                          } else {
+                            if (cardIndex < 2) {
+                              cardIndex++;
+                              colorTween = ColorTween(
+                                  begin: currentColor,
+                                  end: appColors[cardIndex]);
+                            }
+                          }
+                          setState(() {
+                            scrollController.animateTo((cardIndex) * 256.0,
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.fastOutSlowIn);
+                          });
+                          colorTween.animate(curvedAnimation);
+                          animationController.forward();
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(),
     );
   }
 }
