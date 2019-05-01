3f46ca568b7d9b708fe1b63e607094966488457d
Kubilay Çakmak <kubilayckmk@gmail.com>
2019-05-01 17:06:51 +0300

    test

--- build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/soru_bank/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/soru_bank/R.java
@@ -8,13 +8,15 @@
 package com.example.soru_bank;
 
 public final class R {
-  public static final class drawable {
-    public static final int launch_background=0x7f010000;
-  }
-  public static final class mipmap {
-    public static final int ic_launcher=0x7f020000;
-  }
-  public static final class style {
-    public static final int LaunchTheme=0x7f030000;
-  }
+    public static final class drawable {
+        public static final int launch_background = 0x7f010000;
+    }
+
+    public static final class mipmap {
+        public static final int ic_launcher = 0x7f020000;
+    }
+
+    public static final class style {
+        public static final int LaunchTheme = 0x7f030000;
+    }
 }
